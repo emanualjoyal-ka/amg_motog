2c94dcd731df5fc7d5932809f768a2530d8e82be
Emanual Joyal <emanualjoyalka@gmail.com>
2026-03-17 22:41:02 +0530

    made some changes to avoid cold start

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,22 @@
+'use client'
 import Brands from "@/src/sections/Brands"
 import CallToAction from "@/src/sections/CallToAction"
 import Hero from "@/src/sections/Hero"
 import HowItWorks from "@/src/sections/HowItWorks"
 import PartsWeHelpSearch from "@/src/sections/PartsWeHelpSearch"
+import axios from "axios"
+import { useEffect } from "react"
 
 
 const page = () => {
+
+   useEffect(() => {
+    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/health`).catch((err) => {
+      console.log("Backend ping failed:", err.message);
+    });
+  }, []);
+
+
   return (
     <div>
      <Hero/>
--- src/constants/apiUrlConstants.ts
+++ src/constants/apiUrlConstants.ts
@@ -1,7 +1,7 @@
 
 //REQUEST FORM  
 export const API_URL={
-    CREATE_REQUEST:"/api/requests/",
+    CREATE_REQUEST:"/api/requests",
     GET_REQUEST:"/api/requests",
     GET_REQUEST_BY_ID:"/api/requests/",
     CREATE_CONTACT:"/api/contacts",
